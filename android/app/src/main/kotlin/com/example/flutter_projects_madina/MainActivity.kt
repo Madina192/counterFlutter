9a217455b7ff169008efbf63450a0904672b932e
Madina <madinaakpaeva000@gmail.com>
2023-10-04 15:28:47 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_projects_madina/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_projects_madina
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: MainApp(),
+    );
+  }
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "$counter",
+                style: const TextStyle(fontSize: 30),
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.greenAccent,
+          title: const Text('My App'),
+        ),
+        drawer: Drawer(),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+                child: const Icon(Icons.add),
+                onPressed: () {
+                  setState(() {
+                    counter++;
+                  });
+                }),
+            FloatingActionButton(
+                child: const Icon(Icons.remove),
+                onPressed: () {
+                  setState(() {
+                    counter--;
+                  });
+                }),
+          ],
+        ));
+  }
+}
